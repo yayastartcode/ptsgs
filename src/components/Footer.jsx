4e0efcd475945bb4e634cf0ffbe8abdade068c3c
yayastartcode <ordertokoviral@gmail.com>
2026-03-11 10:03:29 +0700

    maps

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -64,7 +64,7 @@ const Footer = () => {
           <div>
             <h3 className="text-xl font-bold mb-4">Lokasi</h3>
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3965.7929585517013!2d106.8034279!3d-6.2909207!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f1e53e3aaaaf%3A0xc77f7ba1fe84ad17!2sJl.%20TB%20Simatupang%20No.kav.%2023-24%2C%20RT.1%2FRW.1%2C%20Cilandak%20Bar.%2C%20Kec.%20Cilandak%2C%20Kota%20Jakarta%20Selatan%2C%20Daerah%20Khusus%20Ibukota%20Jakarta%2012430!5e0!3m2!1sid!2sid!4v1765791076889!5m2!1sid!2sid"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d328.2106932471461!2d107.11638202869706!3d-6.260311990638627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69856e4c6a63ff%3A0x560ec1a6ac90820e!2sJl.%20Telaga%20Murni%20Raya%20Blok%20C10A%20No.12%2C%20Telagamurni%2C%20Kec.%20Cikarang%20Bar.%2C%20Kabupaten%20Bekasi%2C%20Jawa%20Barat%2017530!5e0!3m2!1sid!2sid!4v1773198169058!5m2!1sid!2sid"
               width="100%"
               height="200"
               style={{ border: 0 }}
