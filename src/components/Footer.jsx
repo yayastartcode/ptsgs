58b6dd04fcbdd9df7a84aba659957ac6366a7b28
yayastartcode <ordertokoviral@gmail.com>
2026-03-11 09:54:22 +0700

    updet almt lg

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -31,7 +31,7 @@ const Footer = () => {
                   <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
                 </svg>
                 <div>
-                  <div className="font-semibold text-primary-light mb-1">Jakarta:</div>
+                  <div className="font-semibold text-primary-light mb-1">Alamat:</div>
                   <span className="text-sm">Telaga murni BLOK C10A
                     CIKARANG BARAT. KAB BEKASI</span>
                 </div>
